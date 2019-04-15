8496ffbdd08afb40ac54d2a8f9666ae3a603c295
68lion <68lion@THE-PHENOMENAL.local>
2019-04-15 11:07:14 -0700

    Initial Commit

---
+++ MyMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMap
+//
+//  Created by 68lion on 4/15/19.
+//  Copyright © 2019 68lion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
